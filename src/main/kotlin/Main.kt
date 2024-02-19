ebae08faa293e5b04019c62c7327990c8b157897
Karenmariaphilip <karenmariaphilip@gmail.com>
2024-02-19 09:13:18 +0300

    added files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main() {
+  var name ="Karen philip"
+    println(name)
+    var phoneNumber = 797985806
+    println(phoneNumber)
+    var weight = 45.0
+    println(weight)
+    var citizen = "kenyan"
+    println(citizen)
+
+
+
+
+}
