290fe28aa92a04a1827c159e58e5996dc20166ab
Karenmariaphilip <karenmariaphilip@gmail.com>
2024-03-25 08:21:17 +0300

    added work

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,34 +1,44 @@
 fun main() {
-    nameFor("lucy", "stacy","ann")
-    adultForHowLong(30)
-    infoFunction(17, 78)
-    namesFrom("daisy", "gloris", "tracy")
-    names("jacy", "mary","janet","hassan")
+    isPalindrome("bravo")
+    information("b")
+    numBers(12, 3,5)
 }
+// no.1
+
+fun information(name:String){
+    var sentence= "Barnie bakes brown bagels and buns"
+    println(sentence.removeSuffix(buildString{}))
+
+    // no.2
+    fun numBers(num1: Int, num2: Int, num3: Int): Array<Int> {
+        var a = num1
+        var b = num2
+        var c = num3
+        return arrayOf(a + b + c)
+        return arrayOf(numBers(num1, num2, num3.countOneBits())
+            return arrayOf(num1, num2, num3.)
 
 
-fun names(name1: String, name2: String, name3: String, name4: String) {
-    var nameArray = arrayOf(name1, name2, name3, name4)
-    println(nameArray.contentToString())
-}
 
-fun nameFor(a1: String, b2: String, c3: String): String {
-    var aArray = arrayOf(a1, b2, c3)
-    return aArray.contentToString()
-}
-fun adultForHowLong(num:Int){
-    var num= num-23
-    var sentence= ("I have been adult for $num years")
-    println(sentence)
-}
-fun infoFunction(num1:Int, num2:Int){
-    var numbers = arrayOf(32,17,4,213,78,43,90,31,3,73,11,158,62)
-    println(numbers.indexOf(158))
-    println(numbers[1] + numbers[4])
-    var newNumber= numbers.sortedArray()
-    println(newNumber.contentToString())
-}
-fun namesFrom(name1:String, name2:String, name3:String):String{
-    var nameArray= arrayOf(name1,name2,name3)
-    return nameArray.contentToString()
+
+    }
+
+    // no.3
+
+    fun volumeSphere(num:Int){
+        var volume= 4/3 * 3.14159 *2*2*2
+        println(volume)
+
+    }    }
+
+
+// no.4
+
+fun isPalindrome(word:String):Boolean {
+    var word = "bravo"
+    if (word == "bravo"){
+        return true
+    } else {
+        return false
+    }
 }
